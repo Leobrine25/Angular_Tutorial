ab68969bc94e6450248431c8ec697062b8ad26ac
Leobrine25 <leovieiraferreira@yahoo.com>
2022-03-22 11:26:41 -0300

    selector help on test.components

--- helloworld/src/app/test/test.component.ts
+++ helloworld/src/app/test/test.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+
+/* selector can be 3 ways:
+  "app-test" as a custom html tag <app-test></app-test>
+  ".app-test" as a custom html class <div class="app-test"></div>
+  "[app-test]" as a custom atribute <div app-test> </div>
+
+*/
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
